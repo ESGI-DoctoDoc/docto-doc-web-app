230b079c724621715b845404a0e1f144d742ec76
corentin lechêne <corentin.lechene.pro@gmail.com>
2025-06-06 23:48:18 +0200

    feat(api): update BASE_API_URL to use VITE_API_BASE_URL from environment variables

--- src/services/medical-concerns/medical-concerns.api.ts
+++ src/services/medical-concerns/medical-concerns.api.ts
@@ -18,8 +18,7 @@ import {
 
 
 export const medicalConcernsApi = () => {
-    const config = useRuntimeConfig()
-    const BASE_API_URL = `${config.public.apiBase}/v1`
+    const BASE_API_URL = `${import.meta.env.VITE_API_BASE_URL}/v1`;
 
     async function fetchMedicalConcerns() {
         return new RequestBuilder(BASE_API_URL)

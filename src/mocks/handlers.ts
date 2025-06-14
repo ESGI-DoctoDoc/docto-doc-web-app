f2583d0432f5a9818b0274e83ba79205ac082838
Abdallah Slimane <slimane.abdallah75@gmail.com>
2025-06-14 17:27:11 +0200

    fix absence dto and comment handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -17,48 +17,48 @@ async function rejectRequest(data: unknown): Promise<{ success: false, errorCode
 }
 
 export const handlers = [
-    http.get('http://localhost:8080/api/v1/doctors/absences', async () => {
-        return HttpResponse.json(await resolveRequest([
-            {
-                id: 'uuid1',
-                date: '',
-                startHour: '09:00',
-                endHour: '12:00',
-                start: '2025-06-03',
-                end: '2025-06-03',
-                description: 'Absence due to personal reasons',
-                createdAt: new Date().toISOString(),
-            },
-            {
-                id: 'uuid2',
-                date: undefined,
-                startHour: '14:00',
-                endHour: '17:00',
-                start: '2025-06-05',
-                end: '2025-06-06',
-                description: 'Medical appointment',
-                createdAt: new Date().toISOString(),
-            },
-            {
-                id: 'uuid3',
-                date: '2025-06-08',
-                description: 'Sick leave',
-                createdAt: new Date().toISOString(),
-            },
-        ]));
-    }),
-    http.post('http://localhost:8080/api/v1/doctors/absences', async ({request}) => {
-        return HttpResponse.json(await resolveRequest({
-            id: 'uuid1234',
-            date: '2025-10-02',
-            startHour: '14:00',
-            endHour: '17:00',
-            start: '2025-10-02T14:00:00Z',
-            end: '2025-10-02T17:00:00Z',
-            description: 'Medical appointment',
-            createdAt: new Date().toISOString(),
-        }));
-    }),
+    // http.get('http://localhost:8080/api/v1/doctors/absences', async () => {
+    //     return HttpResponse.json(await resolveRequest([
+    //         {
+    //             id: 'uuid1',
+    //             date: '',
+    //             startHour: '09:00',
+    //             endHour: '12:00',
+    //             start: '2025-06-03',
+    //             end: '2025-06-03',
+    //             description: 'Absence due to personal reasons',
+    //             createdAt: new Date().toISOString(),
+    //         },
+    //         {
+    //             id: 'uuid2',
+    //             date: undefined,
+    //             startHour: '14:00',
+    //             endHour: '17:00',
+    //             start: '2025-06-05',
+    //             end: '2025-06-06',
+    //             description: 'Medical appointment',
+    //             createdAt: new Date().toISOString(),
+    //         },
+    //         {
+    //             id: 'uuid3',
+    //             date: '2025-06-08',
+    //             description: 'Sick leave',
+    //             createdAt: new Date().toISOString(),
+    //         },
+    //     ]));
+    // }),
+    // http.post('http://localhost:8080/api/v1/doctors/absences', async ({request}) => {
+    //     return HttpResponse.json(await resolveRequest({
+    //         id: 'uuid1234',
+    //         date: '2025-10-02',
+    //         startHour: '14:00',
+    //         endHour: '17:00',
+    //         start: '2025-10-02T14:00:00Z',
+    //         end: '2025-10-02T17:00:00Z',
+    //         description: 'Medical appointment',
+    //         createdAt: new Date().toISOString(),
+    //     }));
+    // }),
 
     http.post('http://localhost:8080/api/v1/doctors/slots', async () => {
         return HttpResponse.json(await resolveRequest({
--- src/services/absences/dto/get-absences.dto.ts
+++ src/services/absences/dto/get-absences.dto.ts
@@ -2,12 +2,12 @@ import {z} from 'zod';
 
 export const getDoctorAbsencesResponseSchema = z.array(z.object({
     id: z.string(),
-    date: z.string().optional(),
-    startHour: z.string().optional(),
-    endHour: z.string().optional(),
-    start: z.string().optional(),
-    end: z.string().optional(),
-    description: z.string().optional(),
+    date: z.union([z.string(), z.null()]).optional(),
+    startHour: z.union([z.string(), z.null()]).optional(),
+    endHour: z.union([z.string(), z.null()]).optional(),
+    start: z.union([z.string(), z.null()]).optional(),
+    end: z.union([z.string(), z.null()]).optional(),
+    description: z.union([z.string(), z.null()]).optional(),
     createdAt: z.string(),
-}))
+}));
 export type GetDoctorAbsencesResponse = z.infer<typeof getDoctorAbsencesResponseSchema>;

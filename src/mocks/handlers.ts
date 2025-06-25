c9fd80a5c03a2dffb8b14c5bb1d42a6efb244735
Abdallah Slimane <slimane.abdallah75@gmail.com>
2025-06-25 20:18:10 +0200

    stripe abd

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,19 +1,19 @@
 import {authMockHandlers} from "~/mocks/handlers/auth.handler";
-import {careTrackingMockHandlers} from "~/mocks/handlers/care-tracking.handler";
+// import {careTrackingMockHandlers} from "~/mocks/handlers/care-tracking.handler";
 // import {medicalConcernsMockHandlers} from "~/mocks/handlers/medical-concerns.handler";
 // import {absencesMockHandlers} from "~/mocks/handlers/absences.handler";
-import {patientsMockHandlers} from "~/mocks/handlers/patients.handler";
-import {appointmentsMockHandlers} from "~/mocks/handlers/appointments.handler";
+// import {patientsMockHandlers} from "~/mocks/handlers/patients.handler";
+// import {appointmentsMockHandlers} from "~/mocks/handlers/appointments.handler";
 // import {slotsMockHandlers} from "~/mocks/handlers/slots.handler";
-import {subscriptionMockHandlers} from "~/mocks/handlers/subscription.handler";
+// import {subscriptionMockHandlers} from "~/mocks/handlers/subscription.handler";
 
 export const handlers = [
     ...authMockHandlers,
-    ...careTrackingMockHandlers,
+    // ...careTrackingMockHandlers,
     // ...medicalConcernsMockHandlers,
     // ...slotsMockHandlers,
     // ...absencesMockHandlers,
-    ...patientsMockHandlers,
-    ...appointmentsMockHandlers,
-    ...subscriptionMockHandlers
+    // ...patientsMockHandlers,
+    // ...appointmentsMockHandlers,
+    // ...subscriptionMockHandlers
 ]

d4229b73bfc8f12ff0afd314767e45209d2b5c60
corentin lechêne <corentin.lechene.pro@gmail.com>
2025-07-10 14:47:40 +0200

    style(MainLayout): adjust notification item spacing for better layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -280,7 +280,7 @@ onUnmounted(() => {
                     <li
                         v-for="notification in filteredNotifications"
                         :key="notification.id"
-                        class="flex justify-between items-center px-4 py-2 rounded-lg bg-gray-100 "
+                        class="flex justify-between items-center space-x-6 px-4 py-2 rounded-lg bg-gray-100 "
                     >
                       <div>
                         <p class="text-sm font-medium">{{ notification.title }}</p>
